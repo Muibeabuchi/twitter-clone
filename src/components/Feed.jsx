db06c63983919ab450d8e7b7beb991050af13b83
Nerdkidchiki <you@example.com>
2023-05-01 01:43:42 +0000

    created the post component
    -created the post component and styled it
    -issues arose  with the userimage being squashed too thin--temporary fix
    by giving the surrounding div a minimum height

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,31 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Jhoe Daniel",
+      username: "Jhoedaniel",
+      userimg:
+        "https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png",
+      img: "https://media.istockphoto.com/id/1350384978/photo/autumn-in-alps-hochk%C3%B6nig-mountain-in-austria.jpg?s=612x612&w=0&k=20&c=Wysit8uWNwdWCRIOBuhxla5EBRM_-JeCCFOQhK4qzsg=",
+      text: "GREAT VIEW AND A HAPPY SUNDAY!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Jhoe Daniel",
+      username: "Jhoedaniel",
+      userimg:
+        "https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png",
+      img: "https://media.istockphoto.com/id/480975754/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=g0FV3fbVGrbKMYH6XIuYp9im_j3fAIe-0CCJhNrGl9U=",
+      text: "JUST CAN NOT GET ENOUGH",
+      timestamp: "30 min ago",
+    },
+  ];
+  const POSTS = posts.map((post) => <Post key={post.id} {...post} />);
   return (
     <div className="xl:ml-[370px] border-l border-r  border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl  ">
       <div className="sticky top-0 z-50 flex items-center justify-between px-3 py-2 bg-white border-b ">
@@ -11,6 +35,7 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {POSTS}
     </div>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,58 @@
+import {
+  ChartBarIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  EllipsisHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+
+export default function Post({
+  id,
+  timestamp,
+  name,
+  username,
+  userimg,
+  img,
+  text,
+}) {
+  return (
+    <div className="flex p-3 border-b border-gray-200 cursor-pointer">
+      <div className="mr-4 min-w-[50px] ">
+        <img
+          src={userimg}
+          alt="user-image"
+          className="block rounded-full cursor-pointer h-11 w-11 hover:brightness-95"
+        />
+      </div>
+      <div className="">
+        <div className="">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-2 whitespace-nowrap">
+              <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                {name}
+              </h4>
+              <span className="sm:text-[15px] text-sm">@{username} - </span>
+              <span className="sm:text-[15px] text-sm hover:underline">
+                {timestamp}
+              </span>
+            </div>
+            <EllipsisHorizontalIcon className="w-10 h-10 hoverEffect hover:bg-sky-100 hover:text-sky-500 xl:ml-8 xl:inline" />
+          </div>
+          {/* post-text */}
+          <p className="text-gray-800 text-[15px] sm:text-base mb-2 ">{text}</p>
+          {/* post-image */}
+          <img src={img} alt="post-image" className="mr-2 rounded-2xl" />
+          {/* icons */}
+          <div className="flex items-center justify-between p-2 text-gray-500">
+            <ChatBubbleOvalLeftEllipsisIcon className="p-2  h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100" />
+            <TrashIcon className="p-2 h-9 w-9 hoverEffect hover:text-red-600 hover:bg-red-100" />
+            <HeartIcon className="p-2 h-9 w-9 hoverEffect hover:text-red-600 hover:bg-red-100" />
+            <ShareIcon className="p-2 h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100" />
+            <ChartBarIcon className="p-2 h-9 w-9 hoverEffect hover:text-sky-500 hover:bg-sky-100" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
