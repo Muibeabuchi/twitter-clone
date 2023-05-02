3642758cd969a0eea959974cccc6c2e71e29c2bc
Nerdkidchiki <you@example.com>
2023-05-02 13:59:00 +0000

    completed authentication flow
    -added a callback to the nextauth config to mutate session date pushed
    to the user
    -dynamically rendered parts of the sidebar component based on user
    authentication and verification
    -added sign in and sign out functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,35 +1,41 @@
-import Image from "next/image";
+import { useSession, signOut } from "next-auth/react";
 import { CameraIcon, FaceSmileIcon } from "@heroicons/react/24/outline";
 
 export default function Input() {
+  const { data: session } = useSession();
+  // console.log(session);
   return (
-    <div className="flex p-3 space-x-3 border-b border-gray-200">
-      <img
-        src="https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png"
-        alt="user-image"
-        className="rounded-full cursor-pointer h-11 w-11 hover:brightness-95"
-      />
-      <div className="w-full divide-y divide-gray-200">
-        <div className="">
-          <textarea
-            className="w-full text-lg tracking-wide border-none placeholder-gray-700 min-h[50px] focus:ring-0 text-gray-700 "
-            rows="2"
-            placeholder="What's happening?"
-          ></textarea>
-        </div>
-        <div className="flex items-center pt-2.5 justify-between">
-          <div className="flex">
-            <CameraIcon className="w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100" />
-            <FaceSmileIcon className="w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100" />
+    <>
+      {session && (
+        <div className="flex p-3 space-x-3 border-b border-gray-200">
+          <img
+            onClick={signOut}
+            src={session.user.image}
+            className="rounded-full cursor-pointer h-11 w-11 hover:brightness-95"
+          />
+          <div className="w-full divide-y divide-gray-200">
+            <div className="">
+              <textarea
+                className="w-full text-lg tracking-wide border-none placeholder-gray-700 min-h[50px] focus:ring-0 text-gray-700 "
+                rows="2"
+                placeholder="What's happening?"
+              ></textarea>
+            </div>
+            <div className="flex items-center pt-2.5 justify-between">
+              <div className="flex">
+                <CameraIcon className="w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100" />
+                <FaceSmileIcon className="w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100" />
+              </div>
+              <button
+                className="px-4 text-white bg-blue-400 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50  "
+                disabled
+              >
+                Tweet
+              </button>
+            </div>
           </div>
-          <button
-            className="px-4 text-white bg-blue-400 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50  "
-            disabled
-          >
-            Tweet
-          </button>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 import SideBarMenuItem from "./SideBarMenuItem";
 import { HomeIcon } from "@heroicons/react/24/solid";
 import {
@@ -13,6 +14,8 @@ import {
 } from "@heroicons/react/24/outline";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+  // console.log(session);
   return (
     <aside className="fixed hidden h-full p-2 xl:items-start sm:flex sm:flex-col xl:ml-24">
       {/* twitter logo */}
@@ -28,34 +31,50 @@ export default function Sidebar() {
       <div className="mt-4 mb-2 xl:items-start">
         <SideBarMenuItem text="Home" Icon={HomeIcon} active="active" />
         <SideBarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SideBarMenuItem text="Notifications" Icon={BellIcon} />
-        <SideBarMenuItem text="Messages" Icon={InboxIcon} />
-        <SideBarMenuItem text="Bookmark" Icon={BookmarkIcon} />
-        <SideBarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SideBarMenuItem text="Profile" Icon={UserIcon} />
-        <SideBarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+        {session && (
+          <>
+            <SideBarMenuItem text="Notifications" Icon={BellIcon} />
+            <SideBarMenuItem text="Messages" Icon={InboxIcon} />
+            <SideBarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+            <SideBarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SideBarMenuItem text="Profile" Icon={UserIcon} />
+            <SideBarMenuItem text="More" Icon={EllipsisHorizontalCircleIcon} />
+          </>
+        )}
       </div>
 
       {/* button */}
-      <button className="hidden w-56 h-12 text-lg font-bold text-white bg-blue-400 rounded-full shadow-md xl:inline hover:brightness-95 ">
-        Tweet
-      </button>
+      {!session ? (
+        <button
+          onClick={signIn}
+          className="hidden w-36 h-12 text-lg font-bold text-white bg-blue-400 rounded-full shadow-md xl:inline hover:brightness-95 "
+        >
+          SignIn
+        </button>
+      ) : (
+        <>
+          <button className="hidden w-56 h-12 text-lg font-bold text-white bg-blue-400 rounded-full shadow-md xl:inline hover:brightness-95 ">
+            Tweet
+          </button>
 
-      {/* mini-profile */}
-      <div className="flex items-center justify-center mt-auto text-gray-700 hoverEffect xl:justify-start">
-        <Image
-          src="https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png"
-          alt="user-image"
-          height={40}
-          width={40}
-          className="rounded-full xl:mr-2"
-        />
-        <div className="hidden leading-5 xl:inline">
-          <h4 className="font-bold">UserName</h4>
-          <p>@usertwitterhandle</p>
-        </div>
-        <EllipsisHorizontalIcon className="hidden h-7 xl:ml-8 xl:inline" />
-      </div>
+          {/* mini-profile */}
+          <div className="flex items-center justify-center mt-auto text-gray-700 hoverEffect xl:justify-start">
+            <img
+            onClick={signOut}
+              src={session.user.image}
+              alt="user-image"
+              height={40}
+              width={40}
+              className="rounded-full xl:mr-2"
+            />
+            <div className="hidden leading-5 xl:inline">
+              <h4 className="truncate font-bold">{session.user.name}</h4>
+              <p>@{session.user.username}</p>
+            </div>
+            <EllipsisHorizontalIcon className="hidden h-7 xl:ml-8 xl:inline" />
+          </div>
+        </>
+      )}
     </aside>
   );
 }
